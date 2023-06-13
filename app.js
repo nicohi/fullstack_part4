faf16a3c37c0d806a2baf2a83684d76a4f7f42fd
Nicolas Hiillos <nicolashiillos@gmail.com>
2023-06-13 12:08:21 +0300

    Complete 4.3

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ const mongoose = require('mongoose')
 const config = require('./utils/config')
 const logger = require('./utils/logger')
 const middleware = require('./utils/middleware')
-const Blog = require('./models/blog')
 const blogsRouter = require('./controllers/blogs')
 
 logger.info('connecting to', config.MONGODB_URI)
