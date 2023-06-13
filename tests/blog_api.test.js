39ac0c05db1c59d710805a9a6618ee1730a8b891
Nicolas Hiillos <nicolashiillos@gmail.com>
2023-06-13 17:05:10 +0300

    Complete 4.9 and 4.10

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -30,6 +30,50 @@ describe('when there is initially some blogs saved', () => {
     expect(response.body).toHaveLength(helper.listWithManyBlogs.length)
   })
 
+  test('verify that blog posts have unique identifier named "id"', async () => {
+    const response = await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+    response.body.map(b => expect(b.id).toBeDefined())
+  })
+
+  describe('viewing of a specific blog', () => {
+    test('succeeds with valid a valid id', async () => {
+    })
+    test('fails with status code 404 if note does not exist', async () => {
+    })
+    test('fails with status code 400 if id is invalid', async () => {
+    })
+  })
+  describe('addition of a new blog', () => {
+    test('succeeds with valid data', async () => {
+      const newBlog = helper.listWithOneBlog[0]
+
+      await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(201)
+        .expect('Content-Type', /application\/json/)
+
+      const response = await api.get('/api/blogs')
+
+      const title = response.body.map(r => r.title)
+      const author = response.body.map(r => r.author)
+      const url = response.body.map(r => r.url)
+
+      expect(response.body).toHaveLength(helper.listWithManyBlogs.length + 1)
+      expect(title).toContain(newBlog.title)
+      expect(author).toContain(newBlog.author)
+      expect(url).toContain(newBlog.url)
+    })
+    test('fails with status code 400 if data is invalid', async () => {
+    })
+  })
+  describe('deletion of a blog', () => {
+    test('succeeds with status code 200', async () => {
+    })
+  })
 })
 
 afterAll(async () => {
