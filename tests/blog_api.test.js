dee3fc2f0e99d36c700c1a8a3045252c1dfb83bb
Nicolas Hiillos <nicolashiillos@gmail.com>
2023-06-13 16:41:54 +0300

    Complete 4.8

---
+++ tests/blog_api.test.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const Blog = require('../models/blog')
+const app = require('../app')
+const helper = require('./test_helper')
+
+const api = supertest(app)
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+  const blogObjects = helper.listWithManyBlogs
+    .map(b => new Blog(b))
+  const promiseArray = blogObjects.map(blog => blog.save())
+  await Promise.all(promiseArray)
+})
+
+describe('when there is initially some blogs saved', () => {
+  test('blogs are returned as json', async () => {
+    await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+  })
+
+  test('all blogs are returned', async () => {
+    const response = await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+    expect(response.body).toHaveLength(helper.listWithManyBlogs.length)
+  })
+
+})
+
+afterAll(async () => {
+  await mongoose.connection.close()
+})
