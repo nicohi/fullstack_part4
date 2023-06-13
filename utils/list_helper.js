c252e3e091f482e9a0cf3d37a40f37daf4387bb1
Nicolas Hiillos <nicolashiillos@gmail.com>
2023-06-13 13:22:05 +0300

    Complete 4.4 and 4.5

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -3,6 +3,21 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  return blogs.reduce((sum, blog) => sum + blog.likes, 0)
+}
+
+const favouriteBlog = (blogs) => {
+  const favourite = blogs.reduce(
+  // eslint-disable-next-line no-unused-vars
+    (liked, { id, url, ...b}) => (!liked || b.likes > liked.likes) ? b : liked,
+    null)
+  console.log(favourite)
+  return favourite
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes,
+  favouriteBlog,
 }
