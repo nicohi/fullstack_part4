a85c62eff57b78b0eae86759aa840c8fdf42b025
Nicolas Hiillos <nicolashiillos@gmail.com>
2023-06-14 21:41:12 +0300

    Complete 4.17

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,13 +1,16 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({})
+                          .populate('user', { username: 1, name: 1})
   response.json(blogs)
 })
 
 blogsRouter.get('/:id', async (request, response) => {
   const blog = await Blog.findById(request.params.id) 
+                          .populate('user', { username: 1, name: 1})
   if (blog) response.json(blog)
   else response.status(404).end()
 })
@@ -25,11 +28,13 @@ blogsRouter.put('/:id', async (request, response) => {
   response.json(updatedBlog)
 })
 
-
 blogsRouter.post('/', async (request, response) => {
-  const blog = new Blog(request.body)
+  const users = await User.find({})
+  const user = users[0]
+  const blog = new Blog({ ...request.body, user: user.id })
 
   const result = await blog.save()
+  await User.findByIdAndUpdate(user.id, { blogs: user.blogs.concat(blog.id) })
   response.status(201).json(result)
 })
 
--- controllers/users.js
+++ controllers/users.js
@@ -10,6 +10,7 @@ const validationError = message => {
 
 usersRouter.get('/', async (request, response) => {
   const users = await User.find({})
+    .populate('blogs', { url: 1, title: 1, author:1 })
   response.json(users)
 })
 
