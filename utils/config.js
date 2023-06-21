5d388795d5d47d917e65476e58a6a496816f0764
Nicolas Hiillos <nicolashiillos@gmail.com>
2023-06-21 11:20:42 +0300

    Complete part5

--- utils/config.js
+++ utils/config.js
@@ -2,10 +2,12 @@ require('dotenv').config()
 
 const PORT = process.env.PORT
 const SECRET = process.env.SECRET
-const MONGODB_URI = process.env.NODE_ENV === 'test' ? process.env.TEST_MONGODB_URI : process.env.MONGODB_URI
+const NODE_ENV = process.env.NODE_ENV 
+const MONGODB_URI = NODE_ENV === 'test' ? process.env.TEST_MONGODB_URI : process.env.MONGODB_URI
 
 module.exports = {
   MONGODB_URI,
   PORT,
   SECRET,
+  NODE_ENV,
 }
